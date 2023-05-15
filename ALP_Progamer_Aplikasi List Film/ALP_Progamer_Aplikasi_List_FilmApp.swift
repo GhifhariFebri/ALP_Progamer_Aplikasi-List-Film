c6bfd4e051a08ab7674cb554a7a0fa71443577de
GhifhariFebri <suriansyahghifari@gmail.com>
2023-05-15 17:58:33 +0700

    Initial Commit

---
+++ ALP_Progamer_Aplikasi List Film/ALP_Progamer_Aplikasi_List_FilmApp.swift
@@ -0,0 +1,17 @@
+//
+//  ALP_Progamer_Aplikasi_List_FilmApp.swift
+//  ALP_Progamer_Aplikasi List Film
+//
+//  Created by MacBook Pro on 15/05/23.
+//
+
+import SwiftUI
+
+@main
+struct ALP_Progamer_Aplikasi_List_FilmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
