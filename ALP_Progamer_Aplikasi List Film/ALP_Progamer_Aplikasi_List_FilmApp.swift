a75c6f57c18114e5b0749cbdb0a8c1edd6607fa1
kennethraff <94583510+kennethraff@users.noreply.github.com>
2023-05-27 11:01:50 +0700

    firebase

--- ALP_Progamer_Aplikasi List Film/ALP_Progamer_Aplikasi_List_FilmApp.swift
+++ ALP_Progamer_Aplikasi List Film/ALP_Progamer_Aplikasi_List_FilmApp.swift
@@ -6,9 +6,15 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct ALP_Progamer_Aplikasi_List_FilmApp: App {
+    
+    init() {
+       FirebaseApp.configure()
+       }
+    
     var body: some Scene {
         WindowGroup {
             ContentView(controller: MovieListController())
